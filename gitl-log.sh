b1b81950050894e189ef922c822ce81ed9fe8b6d
Marcus Westin <narcvs@gmail.com>
2015-05-11 15:28:44 -0400

    Allow for passing in flags to gitl-log

--- gitl-log.sh
+++ gitl-log.sh
@@ -1,6 +1,1 @@
-if [ -z "$*" ]
-then
-	git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --
-else
-	git log "$*"
-fi
+git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit "$*" --
