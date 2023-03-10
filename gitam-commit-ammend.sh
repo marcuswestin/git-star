88c4cd5ee2622e382eda8d2b89489c4bff5a9b7c
Marcus Westin <marcus.westin@gmail.com>
2023-03-10 10:51:45 -0500

    Allow for commits without comments

--- gitam-commit-ammend.sh
+++ gitam-commit-ammend.sh
@@ -2,9 +2,10 @@
 
 if [[ $# -eq 0 ]]; then
 	git commit --amend
-elif [[ $# -eq 1 ]]; then
-	git commit --amend -m "$1"
 else
-	echo "Illegal number of parameters"
-	exit -1
+# elif [[ $# -eq 1 ]]; then
+	git commit --amend -m "$1"
+# else
+# 	echo "Illegal number of parameters"
+# 	exit -1
 fi
--- gitm-commit.sh
+++ gitm-commit.sh
@@ -2,9 +2,10 @@
 
 if [[ $# -eq 0 ]]; then
 	git commit
-elif [[ $# -eq 1 ]]; then
-	git commit -m "$1"
 else
-	echo "Illegal number of parameters"
-	exit -1
+# elif [[ $# -eq 1 ]]; then
+	git commit -m "$1"
+# else
+	# echo "Illegal number of parameters"
+	# exit -1
 fi
