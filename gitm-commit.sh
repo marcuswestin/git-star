d60e4028fc03b2448df85adcedae345603bf5f02
Marcus Westin <marcus.westin@gmail.com>
2023-03-24 00:23:17 -0400

    /usr/local/bin/gitm-commit

--- gitm-commit.sh
+++ gitm-commit.sh
@@ -3,8 +3,9 @@
 if [[ $# -eq 0 ]]; then
 	git commit
 else
+	git commit -m "$0"
 # elif [[ $# -eq 1 ]]; then
-	git commit -m "$1"
+# 	git commit -m "$1"
 # else
 	# echo "Illegal number of parameters"
 	# exit -1
