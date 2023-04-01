15f747b7bb2c9feb60e9c8873dda82bfcaa6139b
Marcus Westin <marcus.westin@gmail.com>
2023-04-01 19:41:58 -0400

    Test

--- gitp-push.sh
+++ gitp-push.sh
@@ -22,7 +22,7 @@ cd "$(git rev-parse --show-toplevel)"
 # fi
 
 # echo "Pushing submodules"
-find . -depth -name .git -exec dirname {} \; 2> /dev/null | sort -n -r | xargs -I{} bash -c "cd {}; echo '- Check status: {}'; export REMOTE='origin'; export BRANCH=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`; echo here \$REMOTE \$BRANCH; git status | grep ahead > /dev/null && { echo - Push {} \$REMOTE \$BRANCH; git push \$REMOTE \$BRANCH; }"
+find . -depth -name .git -exec dirname {} \; 2> /dev/null | sort -n -r | xargs -I{} bash -c "cd {}; echo '  - Check {}'; export REMOTE='origin'; export BRANCH=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`; git status | grep ahead > /dev/null && { echo - Push {} \$REMOTE \$BRANCH; git push \$REMOTE \$BRANCH; }"
 
 
 
