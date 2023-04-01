56b3b878802f2392683381bcb7c94b3b543a640e
Marcus Westin <marcus.westin@gmail.com>
2023-04-01 19:40:33 -0400

    again

--- gitp-push.sh
+++ gitp-push.sh
@@ -25,5 +25,6 @@ cd "$(git rev-parse --show-toplevel)"
 find . -depth -name .git -exec dirname {} \; 2> /dev/null | sort -n -r | xargs -I{} bash -c "cd {}; echo '- Check status: {}'; export REMOTE='origin'; export BRANCH=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`; echo here \$REMOTE \$BRANCH; git status | grep ahead > /dev/null && { echo - Push {} \$REMOTE \$BRANCH; git push \$REMOTE \$BRANCH; }"
 
 
+
 # echo "Pushing main"
 # git push $REMOTE $BRANCH
